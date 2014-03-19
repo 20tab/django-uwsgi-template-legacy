9edfd8e2bcd4392bb2e40439375dd7783088f991
20tabsrl <20tab@powertab.local>
2014-03-19 17:01:41 +0100

    templates_dirs in settings

--- project_name/settings.py
+++ project_name/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'south',
 ]
 
-TEMPLATES = (
+TEMPLATE_DIRS = (
     'templates'
 )
 
--- project_name/urls.py
+++ project_name/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-if settings.DEBUG:
+if 'debug_toolbar' in settings.INSTALLED_APPS:
     import debug_toolbar
     urlpatterns += patterns('',
         url(r'^__debug__/', include(debug_toolbar.urls)),
