e1ec1d0c39cf645b7b833360dd47252f2060b3fd
Rafleze <raffaele@20tab.com>
2016-01-12 16:05:33 +0100

    Update __init__.py

--- project_name/settings/__init__.py
+++ project_name/settings/__init__.py
@@ -1,8 +1,8 @@
 import os
 
-if "www/{{ project_name }}" in os.getcwd():
-    from {{ project_name }}.settings.master import *
-elif "www/{{ project_name }}_dev" in os.getcwd():
+if "www/{{ project_name }}_dev" in os.getcwd():
     from {{ project_name }}.settings.develop import *
+elif "www/{{ project_name }}" in os.getcwd():
+    from {{ project_name }}.settings.master import *
 else:
     from {{ project_name }}.settings.local import *
