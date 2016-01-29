766ca83bd2701c914d2139090b997b6a798d1b57
Rafleze <raffaele@20tab.com>
2016-01-29 18:02:59 +0100

    flake8 requirement added

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from fabric.api import local, run, env, cd
 from fabric.contrib.console import confirm
 from fabric.contrib.files import sed
@@ -9,8 +11,8 @@ import os
 
 BASE_DIR = os.path.dirname(__file__)
 PROJECT_DIRNAME = os.path.basename(os.path.dirname(__file__))
-VENVS_DIRNAME = u"{}/venvs".format(os.path.dirname(BASE_DIR))
-VASSALS = u"{}/vassals".format(os.path.dirname(BASE_DIR))
+VENVS_DIRNAME = "{}/venvs".format(os.path.dirname(BASE_DIR))
+VASSALS = "{}/vassals".format(os.path.dirname(BASE_DIR))
 
 output['everything'] = False
 port = '22'
@@ -28,30 +30,34 @@ db_remote = DATABASES['default']
 
 
 def configure_project():
-    output[u'everything'] = True
+    output['everything'] = True
     venv = prompt(
-        u'Specifica il percorso della directory per il virtualenv oppure lascia vuoto per installarlo dentro {}'.format(
+        'Specifica il percorso della directory per il virtualenv oppure lascia vuoto per installarlo dentro {}'.format(
             VENVS_DIRNAME))
     if not venv:
         venv = VENVS_DIRNAME
-    vassals = prompt(u'Specifica il percorso della directory per i vassals oppure lascia vuoto per usare {}'.format(
+    vassals = prompt('Specifica il percorso della directory per i vassals oppure lascia vuoto per usare {}'.format(
         VASSALS))
     if not vassals:
         vassals = VASSALS
 
-    if not os.path.exists(u"{}/{}".format(venv, PROJECT_DIRNAME)):
-        local(u"virtualenv {}/{}".format(venv, PROJECT_DIRNAME))
-        local(u"{}/{}/bin/pip install -r {}/requirements.txt".format(venv, PROJECT_DIRNAME, BASE_DIR))
-    if not os.path.exists(u'templates'):
-        local(u'mkdir templates')
-    if not os.path.exists(u'static'):
-        local(u'mkdir static')
-    if not os.path.exists(u'media'):
-        local(u'mkdir media')
-    if not os.path.exists(u'{}/{}.ini'.format(vassals, PROJECT_DIRNAME)):
-        local(u'ln -s {}/uwsgiconf/locals/{}.ini {}/{}.ini'.format(BASE_DIR, PROJECT_DIRNAME, vassals, PROJECT_DIRNAME))
+    if not os.path.exists("{}/{}".format(venv, PROJECT_DIRNAME)):
+        local("virtualenv {}/{}".format(venv, PROJECT_DIRNAME))
+        local("{}/{}/bin/pip install -r {}/requirements.txt".format(venv, PROJECT_DIRNAME, BASE_DIR))
+    if not os.path.exists('templates'):
+        local('mkdir templates')
+    if not os.path.exists('static'):
+        local('mkdir static')
+    if not os.path.exists('media'):
+        local('mkdir media')
+    if not os.path.exists('{}/{}.ini'.format(vassals, PROJECT_DIRNAME)):
+        local('ln -s {}/uwsgiconf/locals/{}.ini {}/{}.ini'.format(BASE_DIR, PROJECT_DIRNAME, vassals, PROJECT_DIRNAME))
 
-    how_db = prompt(u'Digita 1 per creare il db, 2 per scaricarlo dal server oppure lascia vuoto per non fare nulla!')
+    local("{}/{}/bin/activate".format(venv, PROJECT_DIRNAME))
+    local('flake8 --install-hook')
+    local('git config flake8.strict true')
+
+    how_db = prompt('Digita 1 per creare il db, 2 per scaricarlo dal server oppure lascia vuoto per non fare nulla!')
     # TODO da implementare db_from_server e create_db con una configurazione migliore
     if how_db == "1":
         # create_db()
@@ -69,11 +75,11 @@ def run_test():
 
 def gitclone(repository):
     output['everything'] = True
-    local(u'git init')
-    local(u'git add -A')
-    local(u"git commit -m 'first commit'")
-    local(u'git remote add origin {}'.format(repository))
-    local(u'git push -u origin master')
+    local('git init')
+    local('git add -A')
+    local("git commit -m 'first commit'")
+    local('git remote add origin {}'.format(repository))
+    local('git push -u origin master')
 
 
 def fastpush(message):
@@ -121,7 +127,7 @@ def media_to_server(debug=True):
         output['everything'] = True
     if confirm("Stai sovrascrivendo tutti i file contenuti in /media/ sul server con quelli in locale. Vuoi procedere? "):
         local("rsync -av --progress --inplace --rsh='ssh -p%s' %s:%s/media/ ./media/" % (port, host, project_dir))
-        fastprint(u"Ricordati che sincronizzando i file media e' necessario sincronizzare anche il database con il comando 'fab db_to_server'.")
+        fastprint("Ricordati che sincronizzando i file media e' necessario sincronizzare anche il database con il comando 'fab db_to_server'.")
 
 
 def media_from_server(debug=True):
@@ -129,7 +135,7 @@ def media_from_server(debug=True):
         output['everything'] = True
     if confirm("Stai sovrascrivendo tutti i file contenuti in /media/ in locale con quelli sul server. Vuoi procedere? "):
         local("rsync -av --progress --inplace --rsh='ssh -p%s' %s:%s/media/ ./media/" % (port, host, project_dir))
-        fastprint(u"Ricordati che sincronizzando i file media e' necessario sincronizzare anche il database con il comando 'fab db_from_server'.")
+        fastprint("Ricordati che sincronizzando i file media e' necessario sincronizzare anche il database con il comando 'fab db_from_server'.")
 
 
 def bkp_to_server(debug=True):
@@ -137,7 +143,7 @@ def bkp_to_server(debug=True):
         output['everything'] = True
     if confirm("Stai sovrascrivendo tutti i file contenuti in /bkp/ sul server con quelli in locale. Vuoi procedere? "):
         local("rsync -av --progress --inplace ./bkp/ %s:%s/bkp/" % (host, project_dir))
-        fastprint(u"Operazione completata.")
+        fastprint("Operazione completata.")
 
 
 def db_from_server(debug=True):
@@ -146,22 +152,22 @@ def db_from_server(debug=True):
     if confirm("Attenzione, in questo modo tutti i dati presenti sul database del tuo computer verranno sovrascritti con quelli del database remoto. Sei sicuro di voler procedere?"):
         with cd(project_dir):
             if "postgresql_psycopg2" in db_local['ENGINE']:
-                print (u'- Engine Postgres')
+                print ('- Engine Postgres')
                 run("python dbdump.py")
                 local("scp -P %s %s:%s/tempdump.sql tempdump.sql" % (port, host, project_dir))
                 run("rm tempdump.sql")
                 local('psql -h %s -p %s -U postgres -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = \'%s\';"' % (db_local['HOST'], db_local['PORT'], db_local['NAME']))
                 local('dropdb --if-exists -h %s -p %s -U postgres %s' % (db_local['HOST'], db_local['PORT'], db_local['NAME']))
-                print (u'- Database %s eliminato' % db_local['NAME'])
+                print ('- Database %s eliminato' % db_local['NAME'])
                 local('createdb -h %s -p %s -U postgres %s' % (db_local['HOST'], db_local['PORT'], db_local['NAME']))
-                print (u'- Database %s creato. carico il dump' % db_local['NAME'])
+                print ('- Database %s creato. carico il dump' % db_local['NAME'])
                 if debug:
                     local('psql -h %s -p %s -U postgres -x -e -E -w -d %s -f tempdump.sql -L /dev/null' % (db_local['HOST'], db_local['PORT'], db_local['NAME']))
                 else:
                     local('psql -h %s -p %s -U postgres --output=/dev/null -q -t -w -d %s -f tempdump.sql -L /dev/null' % (db_local['HOST'], db_local['PORT'], db_local['NAME']))
                 local('rm tempdump.sql')
             elif "sqlite" in db_remote['ENGINE']:
-                fastprint(u'- Engine SQLite')
+                fastprint('- Engine SQLite')
                 local("scp -P %s %s:%s/%s %s" % (port, host, project_dir, db_remote['NAME'], db_local['NAME']))
 
 
@@ -171,7 +177,7 @@ def create_db(debug=True):
     if confirm("Attenzione, stai creando il db. Sei sicuro di voler procedere?"):
         if "postgresql_psycopg2" in db_local['ENGINE']:
             local('createdb -h %s -p %s -U postgres %s' % (db_local['HOST'], db_local['PORT'], db_local['NAME']))
-            print (u'- Database %s creato. carico il dump' % db_local['NAME'])
+            print ('- Database %s creato. carico il dump' % db_local['NAME'])
 
 
 def touch():
