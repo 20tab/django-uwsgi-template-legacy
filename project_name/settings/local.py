3d6ce3151e84da9b952f3e5928c5e28fe81b4dd5
rafleze <raffaele@20tab.com>
2016-02-05 12:10:11 +0100

    ignore dirs config

--- project_name/settings/local.py
+++ project_name/settings/local.py
@@ -24,5 +24,5 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = False
 SHOW_TOOLBAR_CALLBACK = True
 
 CLONEDIGGER_CONFIG = {
-    'IGNORE_DIRS': []
+    'IGNORE_DIRS': ['fabfile.py', 'manage.py', '{{project_name}}','migrations',]
 }
