f854fbd0fc10b4b30895b9855dc03f612dfca4ac
Gabriele Giaccari <gabriele@20tab.com>
2016-05-12 16:32:45 +0200

    Delete test_context_processors.py

--- commons/test_context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from commons.context_processors import metainfo, CURRENT_ID
-
-
-class CommonsTest(TestCase):
-
-    def test_metainfo(self):
-        assert CURRENT_ID == metainfo(None)['uuid']
