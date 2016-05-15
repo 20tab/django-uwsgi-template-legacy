4ce9f614b7e78e924fbb10d588588322160e46ec
Gabriele Giaccari <gabriele@20tab.com>
2016-05-15 19:16:38 +0200

    Update testing.py

--- project_name/settings/testing.py
+++ project_name/settings/testing.py
@@ -3,7 +3,7 @@ from {{ project_name }}.settings.base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql',  # Add 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '{{ project_name }}', #os.path.dirname(__file__) + "/../dev.db",#'/path/example.db'. Path to database file if using sqlite3.
         'USER': 'user',  # Not used with sqlite3.
         'PASSWORD': 'password',  # Not used with sqlite3.
