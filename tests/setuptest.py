23cce772ca92a00e7e19193a11e2b825b95146ee
Rafleze <raffaele@20tab.com>
2016-06-09 11:48:53 +0200

    Create setuptest.py

---
+++ tests/setuptest.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+from django.test import TestCase
+
+
+class SetupTestCase(TestCase):
+
+    def setUp(self):
+        """
+        Here put all fixtures required in all tests
+        """
+        pass
+
+    def tearDown(self):
+        pass
