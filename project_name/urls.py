47a5a846504e3e356bbd30a34c2a55f11ab0c0c3
Paolo Melchiorre <paolo@snap.it>
2016-09-01 18:10:07 +0200

    Alignment with Django 1.10

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,17 +1,31 @@
-from django.conf.urls import include, url
-from django.conf import settings
+"""{{ project_name }} URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
 from django.contrib import admin
-admin.autodiscover()
+from django.conf import settings
+
 urlpatterns = [
     # Examples:
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls))
+    url(r'^admin/', admin.site.urls),
 ]
 
 if 'debug_toolbar' in settings.INSTALLED_APPS:
     import debug_toolbar
     urlpatterns.append(
-        url(r'^__debug__/', include(debug_toolbar.urls))
+        url(r'^__debug__/', debug_toolbar.urls)
     )
