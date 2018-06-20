2bb89feef56af23c5039cd7dffdaa51250850526
Paolo Melchiorre <paolo@20tab.com>
2018-06-20 15:48:03 +0200

    Fixed typos, isort and added source virtualenv

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,7 @@ from fabric.contrib.console import confirm
 from fabric.operations import prompt
 from fabric.state import output
 from fabric.utils import fastprint
+
 from {{project_name}}.settings.local import DATABASES as LOCAL_DB
 
 BASE_DIR = os.path.dirname(__file__)
@@ -20,13 +21,15 @@ db_local = LOCAL_DB['default']
 
 
 def init():
-    venvs = prompt(f'Installeremo il virtualenv dentro "{VENVS}", oppure specifica il percorso'.format()) or VENVS
-    vassals = prompt(f'Useremo "{VASSALS}" come directory per i vassals, oppure specifica il percorso') or VASSALS
-    py_version = prompt(f'Useremo {PY_VERSION}, oppure specifica un\'altra versione (es: python2.7)') or PY_VERSION
+    venvs = prompt(f'Installeremo il virtualenv dentro "{VENVS}", oppure specifica il percorso:') or VENVS
+    vassals = prompt(f'Useremo "{VASSALS}" come directory per i vassals, oppure specifica il percorso:') or VASSALS
+    py_version = prompt(f'Useremo {PY_VERSION}, oppure specifica un\'altra versione (es: python2.7):') or PY_VERSION
     if not os.path.exists(f'{venvs}/{PROJECT_DIRNAME}'):
-        local(f'virtualenv -p {py_version} {venvs}/{PROJECT_DIRNAME}'')
+        local(f'virtualenv -p {py_version} {venvs}/{PROJECT_DIRNAME}')
         local(f'{venvs}/{PROJECT_DIRNAME}/bin/pip install -U pip')
         local(f'{venvs}/{PROJECT_DIRNAME}/bin/pip install -r {BASE_DIR}/requirements/dev.txt')
+    else:
+        local(f'. {venvs}/{PROJECT_DIRNAME}/bin/activate')
     if not os.path.exists('templates'):
         local('mkdir templates')
     if not os.path.exists('static'):
